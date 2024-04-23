3913df17b95e3f27d4c423f871b31bafa3f96ce1
odobren <167509358+odobren@users.noreply.github.com>
2024-04-23 15:43:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-const fileInput = document.getElementById('file-input');
-const uploadButton = document.getElementById('upload-button');
-const loadingContainer = document.getElementById('loading-container');
+const fileInput = document.getElementById('fileInput');
+const uploadButton = document.getElementById('uploadButton');
+const loadingContainer = document.getElementById('loadingContainer');
 
 uploadButton.addEventListener('click', () => {
     const files = fileInput.files;
 
-    if (files.length === 0) {
-        alert('Пожалуйста, выберите файл для загрузки.');
-        return;
-    }
+    if (files.length > 0) {
+        loadingContainer.style.display = 'block';
 
-    loadingContainer.style.display = 'block';
+        const formData = new FormData();
 
-    const formData = new FormData();
-    for (const file of files) {
-        formData.append('file', file);
-    }
+        for (const file of files) {
+            formData.append('file', file);
+        }
 
-    fetch('https://script.google.com/macros/s/AKfycbw5U19DJy6Plkuuf1bY6OQZktK-iT4bBv_4rSM5KBhCOCERXsSkzMVWLXpU0YEsME3f/exec', {
-        method: 'POST',
-        body: formData
-    })
+        fetch('https://script.google.com/macros/s/AKfycbw5U19DJy6Plkuuf1bY6OQZktK-iT4bBv_4rSM5KBhCOCERXsSkzMVWLXpU0YEsME3f/exec', {
+            method: 'POST',
+            body: formData
+        })
         .then(response => response.json())
         .then(data => {
+            console.log(data);
             loadingContainer.style.display = 'none';
-            alert('Файлы успешно загружены!');
         })
         .catch(error => {
-            loadingContainer.style.display = 'none';
             console.error(error);
-            alert('Ошибка загрузки файла.');
+            loadingContainer.style.display = 'none';
         });
+    }
 });
