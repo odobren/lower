065c73aa28ea1df030697eea5bea484320d22e91
odobren <167509358+odobren@users.noreply.github.com>
2024-04-23 17:28:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-$(document).ready(function() {
-  $('#uploadForm').submit(function(e) {
-    e.preventDefault();
+$(document).ready(function () {
+    $('#uploadBtn').click(function () {
+        var fileInput = document.getElementById('fileInput');
+        var file = fileInput.files[0];
+        if (!file) {
+            alert('Please select a file.');
+            return;
+        }
+        var formData = new FormData();
+        formData.append('file', file);
 
-    var formData = new FormData();
-    var files = $('#fileInput')[0].files;
+        $('#loader').show(); // Show loader animation
 
-    for (var i = 0; i < files.length; i++) {
-      formData.append('file', files[i]);
-    }
-
-    $('#loading').show();
-
-    $.ajax({
-      url: 'https://script.google.com/macros/s/AKfycbw5U19DJy6Plkuuf1bY6OQZktK-iT4bBv_4rSM5KBhCOCERXsSkzMVWLXpU0YEsME3f/exec',
-      method: 'POST',
-      data: formData,
-      processData: false,
-      contentType: false,
-      success: function(response) {
-        $('#loading').hide();
-        $('#message').text('Файлы успешно загружены!');
-        $('#fileInput').val(''); // Clear the file input
-      },
-      error: function(xhr, status, error) {
-        console.error(xhr.responseText);
-        $('#loading').hide();
-        $('#message').text('Произошла ошибка при загрузке файлов.');
-      }
+        $.ajax({
+            url: 'https://script.google.com/macros/s/AKfycbw5U19DJy6Plkuuf1bY6OQZktK-iT4bBv_4rSM5KBhCOCERXsSkzMVWLXpU0YEsME3f/exec',
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function (response) {
+                $('#loader').hide(); // Hide loader animation
+                alert('File uploaded successfully!');
+                console.log(response);
+            },
+            error: function (jqXHR, textStatus, errorMessage) {
+                $('#loader').hide(); // Hide loader animation
+                console.log(errorMessage); // Log any errors
+                alert('Error uploading file. Please try again.');
+            }
+        });
     });
-  });
 });
