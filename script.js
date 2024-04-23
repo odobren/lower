857090d97ca42cb2a56085a29a3d7805a2590a91
odobren <167509358+odobren@users.noreply.github.com>
2024-04-23 14:53:05 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function uploadFile() {
   const fileInput = document.getElementById('fileInput');
-  const loader = document.getElementById('loader');
+  const loadingAnimation = document.getElementById('loadingAnimation');
 
   const file = fileInput.files[0];
   if (!file) {
-    alert("Please select a file.");
+    alert('Please select a file.');
     return;
   }
 
   const formData = new FormData();
   formData.append('file', file);
 
-  loader.style.display = 'block';
+  loadingAnimation.style.display = 'block';
 
   fetch('https://script.google.com/macros/s/AKfycbw5U19DJy6Plkuuf1bY6OQZktK-iT4bBv_4rSM5KBhCOCERXsSkzMVWLXpU0YEsME3f/exec', {
     method: 'POST',
@@ -19,11 +19,13 @@ function uploadFile() {
   })
   .then(response => response.json())
   .then(data => {
-    loader.style.display = 'none';
-    alert("File uploaded successfully. Link: " + data.link);
+    loadingAnimation.style.display = 'none';
+    alert('File uploaded successfully!');
+    console.log('File uploaded:', data);
   })
   .catch(error => {
-    loader.style.display = 'none';
-    alert("An error occurred: " + error.message);
+    loadingAnimation.style.display = 'none';
+    console.error('Error uploading file:', error);
+    alert('An error occurred while uploading the file. Please try again.');
   });
 }
