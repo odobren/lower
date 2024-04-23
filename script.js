fd0396571f1200f0543b6e24450e5139f8aee257
odobren <167509358+odobren@users.noreply.github.com>
2024-04-23 15:36:54 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
-document.getElementById('uploadButton').addEventListener('click', function() {
-  var fileInput = document.getElementById('fileInput');
-  var file = fileInput.files[0];
-  if (!file) {
-    alert('Please select a file.');
-    return;
-  }
+function uploadFile() {
+    var fileInput = document.getElementById('fileInput');
+    var file = fileInput.files[0];
+    var formData = new FormData();
+    formData.append('file', file);
 
-  var formData = new FormData();
-  formData.append('file', file);
+    var loading = document.getElementById('loading');
+    loading.style.display = 'block'; // Показываем анимацию загрузки
 
-  var loader = document.getElementById('loader');
-  loader.style.display = 'block';
-
-  fetch('https://script.google.com/macros/s/AKfycbw5U19DJy6Plkuuf1bY6OQZktK-iT4bBv_4rSM5KBhCOCERXsSkzMVWLXpU0YEsME3f/exec', {
-    method: 'POST',
-    body: formData
-  })
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('resultMessage').textContent = data;
-    document.getElementById('viewResultButton').style.display = 'block'; // Показываем кнопку "Посмотреть результат"
-    loader.style.display = 'none'; // Скрываем анимированную загрузку после завершения
-  })
-  .catch(error => {
-    console.error('Error:', error);
-    loader.style.display = 'none'; // Скрываем анимированную загрузку в случае ошибки
-  });
-});
+    fetch('https://script.google.com/macros/s/AKfycbw5U19DJy6Plkuuf1bY6OQZktK-iT4bBv_4rSM5KBhCOCERXsSkzMVWLXpU0YEsME3f/exec', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('File uploaded successfully:', data);
+        loading.style.display = 'none'; // Скрываем анимацию после завершения загрузки
+    })
+    .catch(error => {
+        console.error('Error uploading file:', error);
+        loading.style.display = 'none'; // Скрываем анимацию в случае ошибки
+    });
+}
