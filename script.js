f43596768027a6c0b80863ce66f12a9de32a8f62
odobren <167509358+odobren@users.noreply.github.com>
2024-04-23 16:07:10 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
-document.getElementById('uploadButton').addEventListener('click', function() {
+function uploadFile() {
     var fileInput = document.getElementById('fileInput');
     var file = fileInput.files[0];
-    if (file) {
-        var formData = new FormData();
-        formData.append('file', file);
+    var formData = new FormData();
+    formData.append('file', file);
 
-        var loader = document.getElementById('loader');
-        loader.style.display = 'block'; // Показываем анимацию загрузки
+    var loader = document.getElementById('loader');
+    loader.style.display = 'block';
 
-        fetch('https://script.google.com/macros/s/AKfycbw5U19DJy6Plkuuf1bY6OQZktK-iT4bBv_4rSM5KBhCOCERXsSkzMVWLXpU0YEsME3f/exec', {
-            method: 'POST',
-            body: formData
-        })
-        .then(response => response.text())
-        .then(data => {
-            // Обновляем Google Sheets с полученной ссылкой на файл
-            updateGoogleSheets(data);
-            loader.style.display = 'none'; // Скрываем анимацию загрузки после завершения
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            loader.style.display = 'none'; // Скрываем анимацию загрузки в случае ошибки
-        });
-    }
-});
-
-function updateGoogleSheets(fileUrl) {
-    var spreadsheetId = '132llDQJRFBF2dtuX16mF5t4p3v-Z6zgmL36uYh2H1wU';
-    var folderId = '1rVx0u2giWedD--e7slojv2617r8umpbO';
-    // Допишите код для обновления Google Sheets с полученной ссылкой на файл
+    fetch('https://script.google.com/macros/s/ТВОЙ_ИД_СКРИПТА/exec', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.text())
+    .then(data => {
+        console.log('File uploaded successfully:', data);
+        loader.style.display = 'none';
+    })
+    .catch(error => {
+        console.error('Error uploading file:', error);
+        loader.style.display = 'none';
+    });
 }
