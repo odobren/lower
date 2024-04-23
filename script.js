68296ade0f6a8d19e50ac95553b3c734d4152fe2
odobren <167509358+odobren@users.noreply.github.com>
2024-04-23 15:48:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-document.getElementById('uploadButton').addEventListener('click', uploadFile);
-
-function uploadFile() {
+document.getElementById('uploadButton').addEventListener('click', function() {
   var fileInput = document.getElementById('fileInput');
   var file = fileInput.files[0];
-
+  
   if (file) {
     var formData = new FormData();
     formData.append('file', file);
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://script.google.com/macros/s/AKfycbw5U19DJy6Plkuuf1bY6OQZktK-iT4bBv_4rSM5KBhCOCERXsSkzMVWLXpU0YEsME3f/exec');
-    
-    xhr.upload.onloadstart = function() {
-      document.getElementById('loader').style.display = 'block';
-    };
-
-    xhr.upload.onloadend = function() {
-      document.getElementById('loader').style.display = 'none';
-    };
-
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState == XMLHttpRequest.DONE) {
-        if (xhr.status == 200) {
-          alert('File uploaded successfully!');
-        } else {
-          alert('Error uploading file. Please try again.');
-        }
-      }
-    };
-
-    xhr.send(formData);
+    uploadFile(formData);
   } else {
-    alert('Please select a file to upload.');
+    alert('Please select a file.');
   }
+});
+
+function uploadFile(formData) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://script.google.com/macros/s/AKfycbw5U19DJy6Plkuuf1bY6OQZktK-iT4bBv_4rSM5KBhCOCERXsSkzMVWLXpU0YEsME3f/exec');
+  
+  xhr.upload.addEventListener('progress', function(event) {
+    var percent = (event.loaded / event.total) * 100;
+    console.log(percent + '% uploaded');
+    // Показываем анимацию загрузки
+    document.getElementById('loader').classList.remove('hidden');
+  });
+  
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      console.log('File uploaded successfully.');
+      // Скрываем анимацию загрузки после завершения загрузки
+      document.getElementById('loader').classList.add('hidden');
+    } else {
+      console.error('Error uploading file.');
+    }
+  };
+  
+  xhr.send(formData);
 }
