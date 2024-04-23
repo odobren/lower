4efa2b35509e64deca70aa7604e5b741935e6638
odobren <167509358+odobren@users.noreply.github.com>
2024-04-23 14:47:08 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ function uploadFile() {
     var fileInput = document.getElementById('fileInput');
     var file = fileInput.files[0];
 
-    var formData = new FormData();
-    formData.append('file', file);
-
     var loader = document.getElementById("loader");
     loader.style.display = "block";
 
+    var formData = new FormData();
+    formData.append('file', file);
+
     fetch('https://script.google.com/macros/s/AKfycbw5U19DJy6Plkuuf1bY6OQZktK-iT4bBv_4rSM5KBhCOCERXsSkzMVWLXpU0YEsME3f/exec', {
         method: 'POST',
-        body: formData
+        body: file
     })
     .then(response => response.text())
     .then(data => {
