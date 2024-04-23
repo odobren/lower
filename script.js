535f57b67397b3ca53e44f68aebf38b5180ee4b8
odobren <167509358+odobren@users.noreply.github.com>
2024-04-23 15:10:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-document.getElementById("uploadButton").addEventListener("click", function() {
-    var fileInput = document.getElementById("fileInput");
-    var file = fileInput.files[0];
-    if (file) {
-        var formData = new FormData();
-        formData.append("file", file);
+document.getElementById('uploadButton').addEventListener('click', async () => {
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
+    
+    if (!file) {
+        alert('Please select a file.');
+        return;
+    }
 
-        var loader = document.getElementById("loader");
-        loader.style.display = "block"; // Показываем анимированную загрузку
+    const formData = new FormData();
+    formData.append('file', file);
 
-        fetch("https://script.google.com/macros/s/AKfycbw5U19DJy6Plkuuf1bY6OQZktK-iT4bBv_4rSM5KBhCOCERXsSkzMVWLXpU0YEsME3f/exec", {
-            method: "POST",
+    const loader = document.getElementById('loader');
+    loader.style.display = 'block'; // Show loader animation during upload
+
+    try {
+        const response = await fetch('https://script.google.com/macros/s/AKfycbw5U19DJy6Plkuuf1bY6OQZktK-iT4bBv_4rSM5KBhCOCERXsSkzMVWLXpU0YEsME3f/exec', {
+            method: 'POST',
             body: formData
-        })
-        .then(response => response.text())
-        .then(data => {
-            // Обрабатываем ответ
-            console.log("Response:", data);
-            document.getElementById("resultMessage").textContent = data;
-            document.getElementById("viewResultButton").style.display = "block"; // Показываем кнопку "Посмотреть результат"
-            loader.style.display = "none"; // Скрываем анимированную загрузку после завершения
-        })
-        .catch(error => {
-            // Обрабатываем ошибку
-            console.error("Error:", error);
-            loader.style.display = "none"; // Скрываем анимированную загрузку в случае ошибки
         });
+        
+        const data = await response.text();
+        document.getElementById("resultMessage").textContent = data;
+        document.getElementById("viewResultButton").style.display = "block"; // Show "View Result" button
+    } catch (error) {
+        console.error('Error:', error);
+    } finally {
+        loader.style.display = 'none'; // Hide loader animation after upload completes or in case of error
     }
 });
