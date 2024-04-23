27b44a9edd7903f5244626d8ed5eabb2a178a45a
odobren <167509358+odobren@users.noreply.github.com>
2024-04-23 14:29:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-function uploadFile() {
-  const fileInput = document.getElementById('fileInput');
-  const file = fileInput.files[0];
-  const formData = new FormData();
-  formData.append('file', file);
+document.getElementById('uploadButton').addEventListener('click', function() {
+    var fileInput = document.getElementById('fileInput');
+    var files = fileInput.files;
+    var formData = new FormData();
 
-  fetch('https://script.google.com/macros/s/AKfycbw5U19DJy6Plkuuf1bY6OQZktK-iT4bBv_4rSM5KBhCOCERXsSkzMVWLXpU0YEsME3f/exec', {
-    method: 'POST',
-    body: formData
-  })
-  .then(response => response.json())
-  .then(data => {
-    if (data.success) {
-      alert('File uploaded successfully!');
-    } else {
-      alert('Error uploading file.');
+    for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        formData.append('file', file);
     }
-  })
-  .catch(error => {
-    console.error('Error:', error);
-    alert('Error uploading file.');
-  });
-}
+
+    var loader = document.getElementById('loader');
+    loader.style.display = 'block';
+
+    fetch('https://script.google.com/macros/s/AKfycbw5U19DJy6Plkuuf1bY6OQZktK-iT4bBv_4rSM5KBhCOCERXsSkzMVWLXpU0YEsME3f/exec', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('resultMessage').textContent = data;
+        document.getElementById('viewResultButton').style.display = 'block';
+        loader.style.display = 'none';
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        loader.style.display = 'none';
+    });
+});
