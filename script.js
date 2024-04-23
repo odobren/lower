02ef96d386d648afd84c1c76260d2af6fc1ac96f
odobren <167509358+odobren@users.noreply.github.com>
2024-04-23 15:07:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-function uploadFile() {
-  const fileInput = document.getElementById('fileInput');
-  const loader = document.getElementById('loader');
-  loader.style.display = 'block';
+document.getElementById("uploadButton").addEventListener("click", function() {
+    var fileInput = document.getElementById("fileInput");
+    var file = fileInput.files[0];
+    if (file) {
+        var formData = new FormData();
+        formData.append("file", file);
 
-  const formData = new FormData();
-  formData.append('file', fileInput.files[0]);
+        var loader = document.getElementById("loader");
+        loader.style.display = "block"; // Показываем анимированную загрузку
 
-  fetch('https://script.google.com/macros/s/AKfycbw5U19DJy6Plkuuf1bY6OQZktK-iT4bBv_4rSM5KBhCOCERXsSkzMVWLXpU0YEsME3f/exec', {
-    method: 'POST',
-    body: formData
-  })
-  .then(response => response.json())
-  .then(data => {
-    loader.style.display = 'none';
-    console.log('File uploaded successfully. Link:', data.link);
-    // You can do something with the link here, like display it to the user
-  })
-  .catch(error => {
-    loader.style.display = 'none';
-    console.error('Error uploading file:', error);
-  });
-}
+        fetch("https://script.google.com/macros/s/AKfycbw5U19DJy6Plkuuf1bY6OQZktK-iT4bBv_4rSM5KBhCOCERXsSkzMVWLXpU0YEsME3f/exec", {
+            method: "POST",
+            body: formData
+        })
+        .then(response => response.text())
+        .then(data => {
+            // Обрабатываем ответ
+            console.log("Response:", data);
+            document.getElementById("resultMessage").textContent = data;
+            document.getElementById("viewResultButton").style.display = "block"; // Показываем кнопку "Посмотреть результат"
+            loader.style.display = "none"; // Скрываем анимированную загрузку после завершения
+        })
+        .catch(error => {
+            // Обрабатываем ошибку
+            console.error("Error:", error);
+            loader.style.display = "none"; // Скрываем анимированную загрузку в случае ошибки
+        });
+    }
+});
