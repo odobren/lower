f856abe9e53d45b4940c973ea2b79c3369f6670b
odobren <167509358+odobren@users.noreply.github.com>
2024-04-23 14:22:23 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
-function uploadFile() {
-  const fileInput = document.getElementById('fileInput');
-  const file = fileInput.files[0];
-  
-  if (file) {
+function uploadFiles() {
+    const fileInput = document.getElementById('fileInput');
+    const files = fileInput.files;
+
+    if (files.length === 0) {
+        alert('Please select at least one file.');
+        return;
+    }
+
     const formData = new FormData();
-    formData.append('file', file);
-    
+    for (let i = 0; i < files.length; i++) {
+        formData.append('file', files[i]);
+    }
+
     fetch('https://script.google.com/macros/s/AKfycbw5U19DJy6Plkuuf1bY6OQZktK-iT4bBv_4rSM5KBhCOCERXsSkzMVWLXpU0YEsME3f/exec', {
-      method: 'POST',
-      body: formData
+        method: 'POST',
+        body: formData
     })
     .then(response => {
-      if (response.ok) {
-        alert('Файл успешно загружен в Google Sheets.');
-      } else {
-        alert('Произошла ошибка при загрузке файла.');
-      }
+        if (response.ok) {
+            alert('Files uploaded successfully!');
+        } else {
+            alert('Failed to upload files.');
+        }
     })
     .catch(error => {
-      console.error('Ошибка:', error);
-      alert('Произошла ошибка при загрузке файла.');
+        console.error('Error uploading files:', error);
+        alert('Error uploading files. Please try again later.');
     });
-  } else {
-    alert('Выберите файл для загрузки.');
-  }
 }
