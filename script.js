a223b294693eb6d273f6761639423f3ffd4d5a27
odobren <167509358+odobren@users.noreply.github.com>
2024-04-23 15:39:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-function uploadFile() {
-    var fileInput = document.getElementById('fileInput');
-    var file = fileInput.files[0];
-    var formData = new FormData();
-    formData.append('file', file);
+const fileInput = document.getElementById('file-input');
+const uploadButton = document.getElementById('upload-button');
+const loadingContainer = document.getElementById('loading-container');
 
-    var loading = document.getElementById('loading');
-    loading.style.display = 'block'; // Показываем анимацию загрузки
+uploadButton.addEventListener('click', () => {
+    const files = fileInput.files;
+
+    if (files.length === 0) {
+        alert('Пожалуйста, выберите файл для загрузки.');
+        return;
+    }
+
+    loadingContainer.style.display = 'block';
+
+    const formData = new FormData();
+    for (const file of files) {
+        formData.append('file', file);
+    }
 
     fetch('https://script.google.com/macros/s/AKfycbw5U19DJy6Plkuuf1bY6OQZktK-iT4bBv_4rSM5KBhCOCERXsSkzMVWLXpU0YEsME3f/exec', {
         method: 'POST',
         body: formData
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log('File uploaded successfully:', data);
-        loading.style.display = 'none'; // Скрываем анимацию после завершения загрузки
-    })
-    .catch(error => {
-        console.error('Error uploading file:', error);
-        loading.style.display = 'none'; // Скрываем анимацию в случае ошибки
-    });
-}
+        .then(response => response.json())
+        .then(data => {
+            loadingContainer.style.display = 'none';
+            alert('Файлы успешно загружены!');
+        })
+        .catch(error => {
+            loadingContainer.style.display = 'none';
+            console.error(error);
+            alert('Ошибка загрузки файла.');
+        });
+});
